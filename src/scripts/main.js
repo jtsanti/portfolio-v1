3177bbf9b831706d57465eed355a4bff719d7044
Justin Santiago <100609855+jtsanti@users.noreply.github.com>
2023-02-22 17:50:24 -0500

    Add form and projects (#4)
    * Added new images
    
    * added hero image and resume files. Decided to use bootstrap after all. Added contact form. Updated navbar to use bootstrap's.
    
    * added form and adjusted mobile-responsiveness
    
    * minor updates

---
+++ src/scripts/main.js
@@ -0,0 +1,43 @@
+// Get and display current year in footer
+document.getElementById("year").innerHTML = new Date().getFullYear();
+
+// Chevron animations
+const nextArrow = document.getElementById('next-arrow');
+const backToTop = document.getElementById('home');
+
+const animateChevron = (time) => {
+    setTimeout(() => {
+        nextArrow.classList.add('fa-bounce');
+    }, time);
+}
+
+const stopAnimateChevron = () => {
+    nextArrow.classList.remove('fa-bounce');
+}
+
+// Close Bootstrap nav menu when clicked on mobile
+const closeNavOnClick = () => {
+    const navItems = document.querySelectorAll('.nav-item')
+    const navBar = document.getElementById('navbarNav')
+    const collapse = new bootstrap.Collapse(navBar, {
+        toggle: false
+    });
+
+    navItems.forEach((link) => {
+        link.addEventListener('click', () => { collapse.toggle() })
+    })
+}
+
+window.onload = () => {
+    animateChevron(2000);
+
+    nextArrow.onclick = () => {
+        stopAnimateChevron();
+    }
+    
+    backToTop.onclick = () => {
+        animateChevron(4000);
+    }
+
+    closeNavOnClick();
+}
